ac218e4799f44c3c927c8b5c2069c35b845e013c
mcpolandm <mmcpolan@terpmail.umd.edu>
2024-12-15 13:46:08 -0500

    intial

---
+++ p4/flask_app/config.py
@@ -0,0 +1,3 @@
+# Stores all configuration values
+SECRET_KEY = 't5EWHDLuhr73HoEz'
+MONGODB_HOST = 'mongodb+srv://mmcpolan:t5EWHDLuhr73HoEz@cluster0.26rlu.mongodb.net/movie_database?retryWrites=true&w=majority&appName=Cluster0'
---
+++ p4/flask_app/models.py
@@ -0,0 +1,31 @@
+from flask_login import UserMixin
+from datetime import datetime
+from . import db, login_manager
+
+
+# TODO: implement
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(username=user_id).first()
+
+# TODO: implement fields
+class User(db.Document, UserMixin):
+    username = db.StringField(unique=True, required=True, max_length=40, min_length=1)
+    email = db.EmailField(unique=True, required=True)
+    password = db.StringField(required=True)
+    profile_pic = db.ImageField()
+
+    # Returns unique string identifying our object
+    def get_id(self):
+        return self.username
+
+
+# TODO: implement fields
+class Review(db.Document):
+    commenter = db.ReferenceField('User', required=True)
+    content = db.StringField(required=True, min_length=1, max_length=500)
+    date = db.StringField(required=True)
+    imdb_id = db.StringField(required=True, min_length=9, max_length=9)
+    movie_title = db.StringField(required=True, min_length=1, max_length=100)
+    image = db.StringField()
+    #Uncomment when other fields are ready for review pictures
